7e3f6d469585532f12a59c955c1c9f590b8bb605
joshbaughmusic <josh.baugh92@gmail.com>
2023-06-12 19:58:05 -0500

    fixed main heading marigin and gave it an id

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -6,7 +6,7 @@ import { Governors } from './components/Governors.js'
 
 
 export const Exomine = () => {
-    return `<h1>Solar System Mining Marketplace</h1>
+    return `<h1 id="heading__main">Solar System Mining Marketplace</h1>
     <section id="top-section" class="flex-container">
         <div class="flex-container" id="selectors-container">
             ${Governors()}
