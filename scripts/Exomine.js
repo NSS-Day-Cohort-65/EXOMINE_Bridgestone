5d442c4b46722b4d53a86167dbc51c02c3fda7a2
joshbaughmusic <josh.baugh92@gmail.com>
2023-06-19 09:47:10 -0500

    changed alert to window alert for now for mvp - commented out other code for html based alert

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -6,15 +6,28 @@ import { GovernorRecruiter } from './components/GovernorRecruiter.js'
 import { Governors } from './components/Governors.js'
 import { TurnCounter } from './components/TurnCounter.js'
 import { Pirates } from './components/Pirates.js'
-import { raidAlertHTMLGen } from './components/RaidAlert.js'
+import { getState } from './api/dataaccess.js'
+// import { raidAlertHTMLGen } from './components/RaidAlert.js'
 
+document.addEventListener("startRaid", CustomEvent => {
+    const state = getState()
 
+    if (state.lastLocationRaided.length > 1 && state.lastGovernorKilled.length > 1) {
+        const placeRaided = state.lastLocationRaided
+        const govKilled = state.lastGovernorKilled
 
+        window.alert(`WARNING: ${placeRaided.toUpperCase()} WAS RAIDED! GOVERNOR ${govKilled.toUpperCase()} HAS BEEN KILLED!`)
+    } else if (state.lastLocationRaided.length > 1) {
+
+        const placeRaided = state.lastLocationRaided
+
+        window.alert(`WARNING: ${placeRaided.toUpperCase()} WAS RAIDED!`)
+    }
+    raidAlert()
+});
 
 export const Exomine = () => {
 
-    let cart = Cart()
-    let x
 
     return `<h1 id="heading__main">Solar System Mining Marketplace</h1>
     ${TurnCounter()}
@@ -38,7 +51,9 @@ export const Exomine = () => {
         ${Pirates()}
     </section>
     <article id="raid-alert-container">
-    ${raidAlertHTMLGen()}
     </article>`
 }
+
+//if adding in none windwo based alert, place this back above last article tag (like 41 as of time of writing) ${raidAlertHTMLGen()} and uncomment the import statment on like 9.
+
 
--- scripts/components/RaidAlert.js
+++ scripts/components/RaidAlert.js
@@ -1,29 +1,48 @@
 import { getState } from "../api/dataaccess.js"
 
+
+//commenting out and rewriting as window alert for time being. Window alert code below the commented code sitting above it.
+
 //write function that checks state for the last raid. If it exists, creates html rep of it with location included
 
-export const raidAlertHTMLGen = () => {
-    const state = getState()
+// export const raidAlertHTMLGen = () => {
+//     const state = getState()
 
-    if (state.lastLocationRaided.length > 1 && state.lastGovernorKilled.length > 1) {
-        const placeRaided = state.lastLocationRaided
-        const govKilled = state.lastGovernorKilled
-        return `<h1 id="raid-alert-text">WARNING: ${placeRaided.toUpperCase()} WAS RAIDED! GOVERNOR ${govKilled.toUpperCase()} HAS BEEN KILLED!</h1>`
-    } else if (state.lastLocationRaided.length > 1) {
-        const placeRaided = state.lastLocationRaided
-        return `<h1 id="raid-alert-text">WARNING: ${placeRaided.toUpperCase()} WAS RAIDED!</h1>`
-    }
-};
+//     if (state.lastLocationRaided.length > 1 && state.lastGovernorKilled.length > 1) {
+//         const placeRaided = state.lastLocationRaided
+//         const govKilled = state.lastGovernorKilled
+//         return `<h1 id="raid-alert-text">WARNING: ${placeRaided.toUpperCase()} WAS RAIDED! GOVERNOR ${govKilled.toUpperCase()} HAS BEEN KILLED!</h1>`
+//     } else if (state.lastLocationRaided.length > 1) {
+//         const placeRaided = state.lastLocationRaided
+//         return `<h1 id="raid-alert-text">WARNING: ${placeRaided.toUpperCase()} WAS RAIDED!</h1>`
+//     }
+// };
 
-//write an event listener for startRaid custom event that launches the raidAlert function
+// //write an event listener for startRaid custom event that launches the raidAlert function
 
-document.addEventListener("startRaid", CustomEvent => {
-    raidAlert()
-}
-)
+// document.addEventListener("startRaid", CustomEvent => {
+//     raidAlert()
+// }
+// )
+
+// const raidAlert = () => {
+
+//     let alarmSound = new Audio('audio/alarm_sound.mp3')
+
+//     const alert = document.querySelector("#raid-alert-container")
+
+//     alarmSound.play()
+
+//     alert.style.display = "block"
+
+//     setTimeout(() => {
+//         alert.style.display = "none"
+//     }, 4000);
+// }
+
 
 const raidAlert = () => {
-
+    
     let alarmSound = new Audio('audio/alarm_sound.mp3')
 
     const alert = document.querySelector("#raid-alert-container")
@@ -34,14 +53,22 @@ const raidAlert = () => {
 
     setTimeout(() => {
         alert.style.display = "none"
-    }, 4000);
+    }, 3000);
 }
 
+document.addEventListener("startRaid", CustomEvent => {
+    const state = getState()
 
-// for testing:
-// document.addEventListener("click", e => {
-//     if (e.target.id === "test") {
-//         document.dispatchEvent( new CustomEvent("startRaid"))
+    if (state.lastLocationRaided.length > 1 && state.lastGovernorKilled.length > 1) {
+        const placeRaided = state.lastLocationRaided
+        const govKilled = state.lastGovernorKilled
 
-//     }
-// })
+        document.window.alert(`WARNING: ${placeRaided.toUpperCase()} WAS RAIDED! GOVERNOR ${govKilled.toUpperCase()} HAS BEEN KILLED!`)
+    } else if (state.lastLocationRaided.length > 1) {
+
+        const placeRaided = state.lastLocationRaided
+
+        document.window.alert(`WARNING: ${placeRaided.toUpperCase()} WAS RAIDED!`)
+    }
+    raidAlert()
+});
