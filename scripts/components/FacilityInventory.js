2fcaba57bde7a14d362169bc9c96248614ae12ae
Rhett Muse <rhett.muse123@gmail.com>
2023-06-19 15:44:55 -0500

    clean up a little

--- scripts/components/FacilityInventory.js
+++ scripts/components/FacilityInventory.js
@@ -81,10 +81,10 @@ export const FacilityInventory = () => {
                         amountSelector =
                             `<form>
                             <label for="quantity--${inventory.mineral_id}"></label>
-                            <input type="number" id="quantity--${inventory.mineral_id}" name="quantity" min="10" max="500" step="10" value="${selectedAmount}">
+                            <input type="number" id="quantity--${inventory.mineral_id}" name="quantity" min="0" max="${inventory.facility_stock}" value="${selectedAmount}">
                         </form>`
                         let addToCartButton = `<button id=${inventory.mineral_id} class="addToCartButton">Add to Cart</button>`
-                        return `<div class="facilityInventory__items"><input id="mineral-radio--${inventory.mineral_id}" type="radio" name="minerals" value="${inventory.mineral_id}" checked>${amountSelector} tonnes of ${inventory.mineralName}</input> ${addToCartButton}</div>`
+                        return `<div class="facilityInventory__items"><input id="mineral-radio--${inventory.mineral_id}" type="radio" name="minerals" value="${inventory.mineral_id}" checked><div>${amountSelector}</div> ${inventory.facility_stock} tonnes of ${inventory.mineralName}</input> ${addToCartButton}</div>`
                     } else {
                         return `<div><input id="mineral-radio--${inventory.mineral_id}" type="radio" name="minerals" value="${inventory.mineral_id}">${inventory.facility_stock} tonnes of ${inventory.mineralName}</input></div>`
                     }
