1a155a4a31e6f25efaa49a4d645089d6ae7ee52d
joshbaughmusic <josh.baugh92@gmail.com>
2023-06-19 09:48:32 -0500

    changed alert to window alert for now for mvp - commented out other code for html based alert

--- scripts/components/RaidAlert.js
+++ scripts/components/RaidAlert.js
@@ -41,34 +41,34 @@ import { getState } from "../api/dataaccess.js"
 // }
 
 
-const raidAlert = () => {
+// const raidAlert = () => {
     
-    let alarmSound = new Audio('audio/alarm_sound.mp3')
+//     let alarmSound = new Audio('audio/alarm_sound.mp3')
 
-    const alert = document.querySelector("#raid-alert-container")
+//     const alert = document.querySelector("#raid-alert-container")
 
-    alarmSound.play()
+//     alarmSound.play()
 
-    alert.style.display = "block"
+//     alert.style.display = "block"
 
-    setTimeout(() => {
-        alert.style.display = "none"
-    }, 3000);
-}
+//     setTimeout(() => {
+//         alert.style.display = "none"
+//     }, 3000);
+// }
 
-document.addEventListener("startRaid", CustomEvent => {
-    const state = getState()
+// document.addEventListener("startRaid", CustomEvent => {
+//     const state = getState()
 
-    if (state.lastLocationRaided.length > 1 && state.lastGovernorKilled.length > 1) {
-        const placeRaided = state.lastLocationRaided
-        const govKilled = state.lastGovernorKilled
+//     if (state.lastLocationRaided.length > 1 && state.lastGovernorKilled.length > 1) {
+//         const placeRaided = state.lastLocationRaided
+//         const govKilled = state.lastGovernorKilled
 
-        document.window.alert(`WARNING: ${placeRaided.toUpperCase()} WAS RAIDED! GOVERNOR ${govKilled.toUpperCase()} HAS BEEN KILLED!`)
-    } else if (state.lastLocationRaided.length > 1) {
+//         document.window.alert(`WARNING: ${placeRaided.toUpperCase()} WAS RAIDED! GOVERNOR ${govKilled.toUpperCase()} HAS BEEN KILLED!`)
+//     } else if (state.lastLocationRaided.length > 1) {
 
-        const placeRaided = state.lastLocationRaided
+//         const placeRaided = state.lastLocationRaided
 
-        document.window.alert(`WARNING: ${placeRaided.toUpperCase()} WAS RAIDED!`)
-    }
-    raidAlert()
-});
+//         document.window.alert(`WARNING: ${placeRaided.toUpperCase()} WAS RAIDED!`)
+//     }
+//     raidAlert()
+// });
