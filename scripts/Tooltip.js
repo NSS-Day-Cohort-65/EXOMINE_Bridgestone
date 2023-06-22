08b7b53244b577463b267ae96223d96037b4b23a
joshbaughmusic <josh.baugh92@gmail.com>
2023-06-22 13:15:48 -0500

    added some text to tooltip

--- scripts/Tooltip.js
+++ scripts/Tooltip.js
@@ -1,9 +1,21 @@
 export const toolTipRender = () => {
     let html = `
     <img id="tooltip-img" class="image" src="images/help.png">
-    <p id="tooltip-text"><span id="tooltip-heading">MINEWARS - HOW TO PLAY:</span><br><br>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."</p>`
-   
+    <p id="tooltip-text"><span id="tooltip-heading">MINEWARS - HOW TO PLAY:</span><br><br>
+
+    Lead your colonies and defend against the onslaught of the space pirate hoard!<br><br>
+
+    Choose a governor and a facility. You can switch these at anytime. Once you have made your selections, you may obtain minerals from the facility for your colony. A turn will pass everytime you purchase minerals.<br><br>
+    
+    Minerals are the lifeblood of colonies and facilities! They keep your colonies and facilities running and can be used to purchase sercurity. Minerals are consumed by each colony for every turn that passes. Additionally, facilities produce more minerals every turn.<br><br>
+
+    Beware! The space pirates won't stop until they have stolen all of your minerals. Every 3 turns, the pirate captain recruits more raiders to further their conquest. Once raiders have been recruited, raids will begin!<br><br>
+
+    Raids will happen at random and will occur at random colonies and facilities. If you have more security at your location than there are raiders, you will successfully defend the location and protect your minerals, but you will lose security forces. The pirates will also lose some of their raiders, though!<br><br>
+
+    If a raid is successful, the attacked colony or facility will loose a random amount of minerals. Additionally, if a colony is attacked, there is a chance one of its governors will be killed! If this happens, make sure to recruit a new one!<br><br>
+
+    God speed and good luck, leader!</div>`
 
     return html
 }
