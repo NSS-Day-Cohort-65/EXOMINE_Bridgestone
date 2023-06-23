c7931f1f68db3255a0a49958ed64bb7a3fc87f0d
Rhett Muse <rhett.muse123@gmail.com>
2023-06-23 14:55:36 -0500

    update appSettings

--- appSettings.js
+++ appSettings.js
@@ -1,8 +1,8 @@
 export const appSettings = {
     pirates: {
-        MAX_PIRATES_TO_ADD_EACH_TURN: 15,
-        MAX: 12,
-        GRACE_PERIOD: 5,
+        MAX_PIRATES_TO_ADD_EACH_TURN: 7,
+        MAX: 7,
+        GRACE_PERIOD: 4,
         FRACTION_OF_MINERALS_TO_TAKE: .7,
         PIRATES_TO_LOSE: .8,
         CHANCE_OF_GOVERNOR_DEATH: 5
